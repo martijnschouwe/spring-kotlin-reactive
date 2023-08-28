951ac106771d486d3d19a891f632bdbf6a436522
martijnschouwe <martijnschouwe@thesting.com>
2023-08-28 13:20:46 +0200

    Initial import

---
+++ src/main/kotlin/nl/agueda/playground/springkotlinreactive/SpringKotlinReactiveApplication.kt
@@ -0,0 +1,11 @@
+package nl.agueda.playground.springkotlinreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinReactiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinReactiveApplication>(*args)
+}
---
+++ src/test/kotlin/nl/agueda/playground/springkotlinreactive/SpringKotlinReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.agueda.playground.springkotlinreactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinReactiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
